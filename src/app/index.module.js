7b51b97d3520f78f75111c7afd4d3f60c207be38
YzpHelios <1229102737@qq.com>
2016-12-14 10:21:22 +0800

    use yeoman to start angular

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('boss', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
